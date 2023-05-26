80e1fbb581021f02ef0bf0c30fa7b4b8cb52131f
TianfengChen <tfchan1999@gmail.com>
2023-05-26 18:44:17 -0500

    finish_thread_control

--- SV_for_Verification/verilog/thread_control.sv
+++ SV_for_Verification/verilog/thread_control.sv
@@ -115,3 +115,254 @@ end
 如果在某个任务内部禁止该任务，这就像时任务的返回语句，但是这也会停止所有由该任务启动的线程。
 任务被调用三次，衍生了三个线程，线程0在#2延时之后禁止了该任务，导致三个线程最终都没完成。
 */
+
+//event
+//event@ is a variable, it can be used to trigger a thread(thread will wait for the event to be triggered)
+
+//event can be triggered by ->, .notify, @
+//trigger event:
+//-> event_name
+//blocked by event:
+//@ event_name
+//wait trigger event:
+//wait(event_name.triggered());
+
+//locked by missing event
+event el,e2; 
+initial begin
+	$display("@% 0t:1:before trigger",$time);
+	->el;
+	//@e2; //sensitive to edge, replaced by level sensitive wait
+	wait (e2. triggered()); 
+	$display("@% 0t:1:after trigger",$time); 
+end 
+initial begin $display("@% 0t:2:before trigger",$time);
+	->e2;
+	//@e1;
+	wait(el. triggered());
+	display("@% 0t:2:after trigger",$time); 
+end
+
+//no need as a global event, but as handle to share with obj
+//event transferred into constructor new()
+
+class Generator;
+	event e;
+
+	function new(event e);
+		this.e = e;
+	endfunction
+
+	task run();
+		fork
+			begin
+				#10;
+				->e;
+				$display("e triggered");
+			end
+		join_none
+	endtask
+endclass
+
+program automatic test;
+	event e;
+	Generator gen;
+
+	initial begin
+		gen = new(e)
+		gen.run();
+		#20;
+		$display("trigger e");
+		->e;
+		wait(gen.e.triggered());
+	end
+endprogram
+
+//wait for many events
+//solution 1: wait wait_trigger fork
+parameter N_GEN = 3;
+event done[N_GEN];
+initial begin
+	//create generator
+	foreach(gen[i]) begin
+		gen[i] = new(done[i]);
+		gen[i].run();
+	end
+	//wait for all generator done
+	foreach(done[i]) begin
+		fork
+			automatic int k = i;
+			wait(done[k].triggered());
+		join_none
+		wait fork;//wait for all fork to finish
+	end
+end
+//solution 2: wait counter
+parameter N_GEN = 3;
+event done[N_GEN];
+int done_num = 0;
+initial begin
+	//create generator
+	foreach(gen[i]) begin
+		gen[i] = new(done[i]);
+		gen[i].run();
+	end
+	foreach(gen[i])begin
+		fork
+			begin
+			automatic int k = i;
+				wait(done[k].triggered());
+				done_num++;
+			end
+		join_none
+		wait(done_num == N_GEN);
+	end
+end
+
+//solution 3: use static variable to count
+class Generator;
+	static int count=0;
+	task run();
+		count++;
+		fork
+			begin
+				//execute
+				count--;
+			end
+		join_none
+	endtask
+endclass
+
+Generator gen[N_GEN];
+initial begin
+	foreach(gen[i])
+		gen[i] = new();
+	foreach(gen[i])
+		gen[i] = new();
+	wait(Generator::count==0);
+end
+
+//semaphore exclussive access(lock)
+//new(num) will initialize a semaphore with num
+//get() will block if semaphore is 0, put() will release semaphore
+//use semaphore to abtain exclussive access to a resource(bus)
+program automatic test(bus.TB bus);
+	semaphore bus_lock;
+	initial begin
+		bus_lock = new(1);
+		fork
+			sequencer();
+			sequencer();//produce two bus event thread
+		join
+	end
+
+	task sequencer();
+		fork
+			sendTrans();
+		join_none
+	endtask
+
+	task sendTrans();
+				//wait for bus_lock
+				bus_lock.get(1);
+				//execute
+				...
+				//send bus transaction
+				bus_lock.put(1);
+	endtask
+endprogram
+
+//mailbox
+//generator---driver
+//avoid shallow copy, use new() to create a new object, parameter is the size of mailbox
+//mailbox is a queue, can be used to transfer object between threads
+//put() will block if mailbox is full, get() will block if mailbox is empty
+
+task generator(mailbox mbx);
+	Transaction t;
+	repeat(10) begin
+		t = new();
+		t.randomize();
+		mbx.put(t);
+	end
+endtask
+
+task driver(mailbox mbx);
+	Transaction t;
+	repeat(10) begin
+		mbx.get(t);
+	end
+endtask
+
+
+//mail box in class and connect generator and driver
+class Generator;
+	mailbox mbx;
+	Transaction t;
+
+	function new(mailbox mbx);
+		this.mbx = mbx;
+	endfunction
+
+	task run(int n);
+		repeat(n)
+		begin
+			tr = new();
+			assert(tr.randomize());
+			mbx.put(tr);
+		end
+	endtask
+endclass
+
+class Driver;
+	mailbox mbx;
+	Transaction t;
+
+	function new(mailbox mbx);
+		this.mbx = mbx;
+	endfunction
+
+	task run(int n);
+		repeat(n) begin
+			mbx.get(t);
+		end
+	endtask
+endclass
+
+initial begin
+	int count = 10;
+	mailbox mbx = new(11);//size of mailbox is 11
+	Generator gen = new(mbx);
+	Driver drv = new(mbx);
+	fork
+		gen.run(count);
+		drv.run(count);
+	join
+end
+
+//asynchronous mailbox
+//peek() will return the next object in mailbox, but not remove it
+//no synchronizor, no blocking
+initial begin
+	mbx = new();//new(1);  //use size 1 mailbox to synchronize
+	fork
+		p.run();//thread 1 will not be blocked until it finished executing
+		c.run();
+	join
+end
+
+//use shakehand event to synchronize
+program automatic skhd_evt;
+	mailbox mbx;
+	event handshake;
+	class Generator;
+		task
+
+//use semaphore to synchronize
+
+//use rx and tx mailbox to synchronize
+
+
+
+
+
